042d4ac09c05be4375d917f8ebf041dbe1b8af54
PatrickPahunang <patrickcd452@gmail.com>
2022-10-15 16:32:35 +0800

    updates

--- SafeSight/SafeSight/settings.py
+++ SafeSight/SafeSight/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'SafeSight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'capstonefinal' ,
+        'HOST' : 'localhost' ,
+        'USER' : 'root' ,
+        'PASSWORD' : '' ,
+        'PORT' : '3306' ,
+        
+        },
     }
-}
+
+
 
 
 # Password validation
