d7b7c9242a523a91cc18d1ed7f7eb93c5f0c79d1
PatrickPahunang <patrickcd452@gmail.com>
2022-10-15 19:43:24 +0800

    latest update

--- SafeSight/SafeSight/urls.py
+++ SafeSight/SafeSight/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from SafeSight import views
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+
+    
 ]
