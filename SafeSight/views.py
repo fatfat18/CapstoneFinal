c082ac0ec4962ae7ab47c3de0f65176379486363
PatrickPahunang <patrickcd452@gmail.com>
2022-10-20 20:55:11 +0800

    SIGN UP BACK END

--- SafeSight/views.py
+++ SafeSight/views.py
@@ -1,47 +1,55 @@
 from ctypes.wintypes import MSG
 from distutils.log import error
+from xml.dom import ValidationErr
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout
 from django.shortcuts import HttpResponseRedirect
+from django.contrib.auth import forms  
+from django.contrib import messages  
+from .forms import CustomUserCreationForm
 
 
 
 
-  
+
 def signup(request):
-    if request.user.is_authenticated:
-        return redirect('/profile')
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password, email=email)
-            login(request, user)
-            return redirect('/')
-        else:
-            msg = 'Please fill in the blanks properly!'
-            return render(request, 'signup.html', {'form': form , 'msg': msg})
-    else:
-        form = UserCreationForm()
-        return render(request, 'signup.html', {'form': form})
+      if request.user.is_authenticated:
+          return redirect('/profile')
+      if request.method == 'POST':
+          form = CustomUserCreationForm(request.POST)
+          if form.is_valid():
+              form.save()
+              username = form.cleaned_data.get('username')
+              email = form.cleaned_data.get('email')
+              password = form.cleaned_data.get('password1')
+              user = authenticate(username=username, password=password, email=email)
+              login(request, user)
+              if request.user.is_authenticated:
+                 return redirect('/profile')
+          else:
+              msg = 'Please fill in the blanks properly!'
+              return render(request, 'signup.html', {'form': form , 'msg': msg})
+      else:
+          form = CustomUserCreationForm()
+          return render(request, 'signup.html', {'form': form})
 
 
 
-def home(request): 
+
+
+
+def home(request):
         return render(request, 'home.html')
-   
-  
+
+
 def signin(request):
     if request.user.is_authenticated:
         return render(request, 'home.html')
     if request.method == 'POST':
-        username = request.POST['username'] 
+        username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
@@ -54,16 +62,16 @@ def signin(request):
     else:
         form = AuthenticationForm()
         return render(request, 'login.html', {'form': form})
-  
-def profile(request): 
+
+def profile(request):
     return render(request, 'profile.html')
-   
+
 def signout(request):
     logout(request)
     return redirect('/signin')
 
-def aboutus(request): 
+def aboutus(request):
     return render(request, 'aboutus.html')
 
-def dashboard(request): 
+def dashboard(request):
     return render(request, 'dashboard.html')
