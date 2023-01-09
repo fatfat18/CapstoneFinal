48a9397bde204c8eee90c536ef7e2f329dd10888
PatrickPahunang <patrickcd452@gmail.com>
2023-01-09 22:22:23 +0800

    test deploy

--- SafeSight/settings.py
+++ SafeSight/settings.py
@@ -139,9 +139,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # STATICFILES_DIRS = ( os.path.join('static'), )
 
-import os
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
